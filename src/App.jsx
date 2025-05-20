00647668095bff08034a94da3c81d77466c8742c
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-05-20 16:45:52 +0530

    App.jsx file  added.

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { createBrowserRouter } from 'react-router-dom'
+import './App.css'
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Pastes from './components/Pastes';
+import View from './components/View';
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element:
+        <div>
+          <Navbar />
+          <Home />
+        </div>
+    },
+    {
+      path: "/pastes",
+      element:
+        <div>
+          <Navbar />
+          <Pastes />
+        </div>
+    },
+    {
+      path: "/pastes/:id",
+      element:
+        <div>
+          <Navbar />
+          <View />
+        </div>
+    },
+  ]
+);
+
+function App() {
+
+
+  return (
+    <div>
+      <RouterProvider router={router}/>
+    </div>
+
+  )
+}
+
+export default App
